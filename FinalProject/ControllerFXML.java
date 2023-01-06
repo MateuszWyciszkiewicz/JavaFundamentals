2adc9397a3e9d901e2930a523cd25c69d7f91c39
Mateusz Wyciszkiewicz <matiwycisz@gmail.com>
2023-01-06 14:09:43 +0100

    UI with javafx

---
+++ FinalProject/ControllerFXML.java
@@ -0,0 +1,34 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ControllerFXML implements Initializable {
+    
+       
+    @FXML 
+    private TextArea QueryTextArea;
+    
+    static TextArea staticTxtArea;
+    
+    @FXML
+    private TextField QueryField;
+         
+    @FXML
+    private void executeQuery(ActionEvent event) { 
+        
+        // these will be redirected to textArea on GUI
+        String query = QueryField.getText();
+        Parser.executeQuery(query);
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+      staticTxtArea = QueryTextArea;
+  }
+      
+}
