5edf3a0fa6a4ff79742abaf661f5c3458c01c170
Mateusz Wyciszkiewicz <matiwycisz@gmail.com>
2023-01-10 19:30:12 +0100

    unused imports

--- FinalProject/Queries/AdvancedQueries/WhereDelete.java
+++ FinalProject/Queries/AdvancedQueries/WhereDelete.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-
 import Exceptions.BadSyntaxException;
 import Queries.BasicQueries.Delete;
 
